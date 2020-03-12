d20309883014d03a440f4ade5df6d5d16bdcbaa7
HerickJhones11 <59341296+HerickJhones11@users.noreply.github.com>
2020-03-11 23:52:02 -0300

    Create itemnotafiscal.py

---
+++ itemnotafiscal.py
@@ -0,0 +1,18 @@
+from  tipocliente import  TipoCliente
+
+from  cliente import Cliente
+
+class ItemNotaFiscal():
+    _valor = 0.0
+
+    def _init_(self, id, sequencial, quantidade, numero):
+        self._id = id
+        self._sequencial = sequencial
+        self._quantidade = quantidade
+        self._numero = numero
+    def valorItemNF(self):
+        pass
+    def toString(self):
+        pass
+
+
