ed6cba3026354ceda4d1faf9855d23ea60091199
HerickJhones11 <59341296+HerickJhones11@users.noreply.github.com>
2020-03-11 23:53:22 -0300

    Create testenotafiscalvenda.py

---
+++ testenotafiscalvenda.py
@@ -0,0 +1,12 @@
+from  cliente import Cliente
+from itemnotafiscal import ItemNotaFiscal
+from notafiscalvenda import  NotaFiscalVenda
+from produto import Produto
+from tipocliente import TipoCliente
+
+#def main():
+#   p1=Produto(1, "arroz01", "Arroz de Goiás", 5.00)
+#  print(p1.str())
+
+##if __name__ == '__main__':
+#    main()
