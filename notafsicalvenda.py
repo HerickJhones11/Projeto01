2d393abce1cfd1ecc2a44a225bc530be6bb6b694
HerickJhones11 <59341296+HerickJhones11@users.noreply.github.com>
2020-03-11 23:52:42 -0300

    Create notafsicalvenda.py

---
+++ notafsicalvenda.py
@@ -0,0 +1,16 @@
+class NotaFiscalVenda:
+    def __init__(self, codigo, data , valorNota, cliente, id):
+        self._codigo = codigo
+        self._data = data
+        self._valorNota = valorNota
+        self._id = id
+        self._cliente = cliente
+
+    def calcularValor(self):
+        pass
+    def criaritemNotaFiscal(self):
+        pass
+    def NotaFiscalVenda(self):
+        pass
+    def toString(self):
+        pass
